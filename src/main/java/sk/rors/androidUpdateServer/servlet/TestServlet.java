aa2162ee5fe362a50563a75d7a44b47b752c5aa1
Jozef Dochan <theglow666@gmail.com>
2018-07-26 23:08:54 +0200

    initial setup of project

---
+++ src/main/java/sk/rors/androidUpdateServer/servlet/TestServlet.java
@@ -0,0 +1,14 @@
+package sk.rors.androidUpdateServer.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().append("Hello, World!");
+    }
+}
