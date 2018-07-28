bd5cf8cd126ae8b12ac50e0a8a2c42a04c9f986c
Jozef Dochan <theglow666@gmail.com>
2018-07-28 19:55:43 +0200

    refactore servlet definitions

--- src/main/java/sk/rors/androidUpdateServer/servlet/TestServlet.java
+++ src/main/java/sk/rors/androidUpdateServer/servlet/TestServlet.java
@@ -1,10 +1,12 @@
 package sk.rors.androidUpdateServer.servlet;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet( name = "TestServlet", urlPatterns = "/")
 public class TestServlet extends HttpServlet {
 
     @Override
