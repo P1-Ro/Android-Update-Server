3df77ecbadcac08aade98356f00dc66c14c27735
Jozef Dochan <theglow666@gmail.com>
2018-07-29 20:27:12 +0200

    servlet for getting info about package nearly finished

--- src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
+++ src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
@@ -1,8 +1,9 @@
 package sk.rors.androidUpdateServer.servlet;
 
-import sk.rors.androidUpdateServer.model.Latest;
+import sk.rors.androidUpdateServer.model.Apk;
+import sk.rors.androidUpdateServer.persistence.Database;
+import sk.rors.androidUpdateServer.util.ErrorResponse;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +14,21 @@ import java.io.IOException;
 public class LatestServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Latest latest = new Latest("1.0", 1);
-        String result = latest.serialize();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        resp.getWriter().append(result);
+        String packageName = req.getParameter("packagename");
+        try{
+            Apk apk = (Apk) Database.getInstance().getDao(Apk.class).queryForId(packageName);
+            if(apk != null){
+                resp.getWriter().append(apk.serialize());
+            } else {
+                resp.getWriter().append(new ErrorResponse("No apk with such packageName", 404).serialize());
+            }
+
+        }catch (Exception e){
+            // TODO better handling of exceptions
+            resp.getWriter().append(e.toString());
+        }
+
     }
 }
