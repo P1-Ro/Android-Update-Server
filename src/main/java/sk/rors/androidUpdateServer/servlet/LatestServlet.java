5947415df3d073955c77d15baef66ef1d2e1248c
Jozef Dochan <theglow666@gmail.com>
2018-07-30 20:15:24 +0200

    finished LatestServlet

--- src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
+++ src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
@@ -10,24 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LatestServlet", urlPatterns = "/latest")
+@WebServlet(name = "LatestServlet", urlPatterns = {"/latest", "/download"})
 public class LatestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         String packageName = req.getParameter("packagename");
-        try{
+        try {
             Apk apk = (Apk) Database.getInstance().getDao(Apk.class).queryForId(packageName);
-            if(apk != null){
-                resp.getWriter().append(apk.serialize());
+            if (apk != null) {
+
+                if (req.getRequestURI().endsWith("/download")) {
+                    resp.setHeader("content-disposition", "attachment; filename=\"" + packageName + ".apk\"");
+                    resp.getOutputStream().write(apk.getApk());
+                } else {
+                    resp.getWriter().append(apk.serialize());
+                }
             } else {
                 resp.getWriter().append(new ErrorResponse("No apk with such packageName", 404).serialize());
             }
 
-        }catch (Exception e){
-            // TODO better handling of exceptions
-            resp.getWriter().append(e.toString());
+        } catch (Exception e) {
+            resp.getWriter().append(new ErrorResponse(e.toString(), 500).serialize());
         }
 
     }
