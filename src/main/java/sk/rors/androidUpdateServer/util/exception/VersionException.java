27934dd50bae21cf448d5766efa5a0df84654bc0
Jozef Dochan <theglow666@gmail.com>
2018-08-07 21:55:41 +0200

    changed parent of versionException

--- src/main/java/sk/rors/androidUpdateServer/util/exception/VersionException.java
+++ src/main/java/sk/rors/androidUpdateServer/util/exception/VersionException.java
@@ -3,7 +3,7 @@ package sk.rors.androidUpdateServer.util.exception;
 /**
  * Class for indicating new application has equal or lower version
  */
-public class VersionException extends RuntimeException {
+public class VersionException extends Exception {
 
     public VersionException(String msg) {
         super(msg);
