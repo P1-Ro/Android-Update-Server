85cbac3c2d1d331f412aed78040701833623e2f1
Jozef Dochan <theglow666@gmail.com>
2018-08-09 23:12:02 +0200

    added doPost with same behaviour improved look of url added correct http status codes

--- src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
+++ src/main/java/sk/rors/androidUpdateServer/servlet/LatestServlet.java
@@ -4,19 +4,21 @@ import sk.rors.androidUpdateServer.model.Apk;
 import sk.rors.androidUpdateServer.persistence.Database;
 import sk.rors.androidUpdateServer.util.ErrorResponse;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LatestServlet", urlPatterns = {"/latest", "/download"})
+@WebServlet(name = "LatestServlet", urlPatterns = {"/latest/*", "/download/*"})
 public class LatestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        String packageName = req.getParameter("packageName");
+        String[] uriParts = req.getRequestURI().split("/");
+        String packageName = uriParts[uriParts.length-1];
         try {
             Apk apk = Database.getInstance().getDao(Apk.class).queryForId(packageName);
             if (apk != null) {
@@ -29,11 +31,17 @@ public class LatestServlet extends HttpServlet {
                 }
             } else {
                 resp.getWriter().append(new ErrorResponse("No apk with such packageName", 404).serialize());
+                resp.setStatus(404);
             }
 
         } catch (Exception e) {
             resp.getWriter().append(new ErrorResponse(e.toString(), 500).serialize());
+            resp.setStatus(500);
         }
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
     }
 }
