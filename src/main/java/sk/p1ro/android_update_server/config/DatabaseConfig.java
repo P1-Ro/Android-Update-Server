ace931c053bd554cdba6f5d6308d9af69a7c764d
P1Ro <theglow666@gmail.com>
2020-08-02 21:12:08 +0200

    fix heroku deployment (finally?)

--- src/main/java/sk/p1ro/android_update_server/config/DatabaseConfig.java
+++ src/main/java/sk/p1ro/android_update_server/config/DatabaseConfig.java
@@ -2,28 +2,48 @@ package sk.p1ro.android_update_server.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url:#{null}}")
-    private String dbUrl;
+    Logger log = LoggerFactory.getLogger(getClass());
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
+        String databaseUrl = System.getenv("DATABASE_URL");
 
-        if (dbUrl == null) {
+        if (databaseUrl == null) {
             config.setDriverClassName("org.h2.Driver");
             config.setJdbcUrl("jdbc:h2:~/testdb");
         } else {
             config.setDriverClassName("org.postgresql.Driver");
+
+            URI dbUri;
+            try {
+                dbUri = new URI(databaseUrl);
+            }
+            catch (URISyntaxException e) {
+                log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+                return null;
+            }
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
             config.setJdbcUrl(dbUrl);
+            config.setUsername(username);
+            config.setPassword(password);
         }
 
         return new HikariDataSource(config);
