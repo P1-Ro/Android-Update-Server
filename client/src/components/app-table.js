35799d75c9066f10601f8dadb4842142932df87e
P1Ro <theglow666@gmail.com>
2020-08-02 20:09:31 +0200

    improve layout on mobile devices

--- client/src/components/app-table.js
+++ client/src/components/app-table.js
@@ -17,16 +17,16 @@ function renderTable(tableData) {
     for (let data of tableData) {
         rows.push(
             <Row key={data.packageName}>
-                <Col sm={3}>
+                <Col xs={3}>
                     <img alt={`Icon of ${data.name}`} src={`data:image/jpeg;base64,${data.icon}`}/>
                 </Col>
-                <Col sm={3} className='valign'>
+                <Col xs={3} className='valign'>
                     {data.name}
                 </Col>
-                <Col sm={3} className='valign'>
+                <Col xs={3} className='valign'>
                     {data.versionName}
                 </Col>
-                <Col sm={3} className='valign'>
+                <Col xs={3} className='valign'>
                     <a href={`/api/download/${data.packageName}`}>Download</a>
                 </Col>
             </Row>
@@ -41,16 +41,16 @@ function renderTable(tableData) {
                 marginBottom: "20px",
                 paddingBottom: "5px"
             }}>
-                <Col sm={3}>
+                <Col xs={3}>
                     Icon
                 </Col>
-                <Col sm={3}>
+                <Col xs={3}>
                     Name
                 </Col>
-                <Col sm={3}>
+                <Col xs={3}>
                     Version
                 </Col>
-                <Col sm={3}/>
+                <Col xs={3}/>
             </Row>
             {rows}
         </Container>
